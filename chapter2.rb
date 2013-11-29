4a3369e314e1c9974017b7c44090545451b279c9
Bart De Wachter <bart@licious.be>
2013-11-29 22:26:02 +0100

    finished chapter 2

--- chapter2.rb
+++ chapter2.rb
@@ -1,3 +1,4 @@
 puts (365 * 24).to_s + " hours in 1 year"
 puts (((365 * 10) * 24) * 60).to_s + " minutes in a decade"
-puts "my age in seconds: " + ((((365 * 33) * 25) * 60) * 60).to_s
+puts "my age in seconds: " + ((((365 * 33) * 24) * 60) * 60).to_s
+puts "author's age: " + (((((1160000000) / 60) / 60) / 24) / 365).to_s
